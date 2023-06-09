f3b5e4d0dccd33ccbcba3309e545fe156ae76880
Leo <leo@netmatch.nl>
2023-06-09 16:05:44 +0200

    Added back fix for Azure OpenAI

--- utils/app/const.ts
+++ utils/app/const.ts
@@ -16,3 +16,6 @@ export const OPENAI_API_VERSION =
 
 export const OPENAI_ORGANIZATION =
   process.env.OPENAI_ORGANIZATION || '';
+
+export const AZURE_DEPLOYMENT_ID =
+  process.env.AZURE_DEPLOYMENT_ID || '';
--- utils/server/index.ts
+++ utils/server/index.ts
@@ -2,6 +2,7 @@ import { OpenAIModel } from '@/types/openai';
 import { Message } from '@chatbot-ui/core/types/chat';
 
 import {
+  AZURE_DEPLOYMENT_ID,
   OPENAI_API_HOST,
   OPENAI_API_TYPE,
   OPENAI_API_VERSION,
@@ -38,7 +39,7 @@ export const OpenAIStream = async (
 ) => {
   let url = `${OPENAI_API_HOST}/v1/chat/completions`;
   if (OPENAI_API_TYPE === 'azure') {
-    url = `${OPENAI_API_HOST}/openai/deployments/${model.id}/chat/completions?api-version=${OPENAI_API_VERSION}`;
+    url = `${OPENAI_API_HOST}/openai/deployments/${AZURE_DEPLOYMENT_ID}/chat/completions?api-version=${OPENAI_API_VERSION}`;
   }
   const res = await fetch(url, {
     headers: {
